e237889d45fc261f81b19f8febe687eb1c8eb6fa
Rémi Prévost <remi@exomel.com>
2019-11-04 08:27:11 -0500

    Fix typo

--- lib/emojibot/bot.ex
+++ lib/emojibot/bot.ex
@@ -4,7 +4,7 @@ defmodule Emojibot.Bot do
 
   def handle_event(%{name: name, type: "emoji_changed", subtype: "add"}, slack, state) do
     emoji_channel = Application.get_env(:emojibot, :emoji_channel)
-    send_message(":#{name}: (`name`)", emoji_channel, slack)
+    send_message(":#{name}: (`#{name}`)", emoji_channel, slack)
 
     {:ok, state}
   end
